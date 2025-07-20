4e8b519ee84e495a5b3173ec2031a2b59d24c4e4
saurav1980 <saurav.choudhury@njclabs.com>
2025-07-20 14:22:14 +0530

    First Push

---
+++ src/main/java/com/example/QRGenerator.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.io.ByteArrayOutputStream;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
+public class QRGenerator {
+
+    public static byte[] generateQRCode(String text, int width, int height) throws Exception {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+        return outputStream.toByteArray();
+    }
+}
